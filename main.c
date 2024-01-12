40693f3bdc9a82e683c9fb6f7e8889bef62c85c5
Jakub Šmejkal <smejkj20@fit.cvut.cz>
2024-01-12 18:48:26 +0100

    Main.c led blink

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
-int main(void)
-{
-  for(;;) {
-    
-  }
-  return 0;   /* Never reached */
-}
+#include "blinz.h"
+
+// Define led pulse output pins
+#define LED_DDR        DDRB
+#define LED_PORT       PORTB
+#define LED_BIT        5  // Uno Digital Pin 13
+
+int main(void) {
+    LED_DDR |= (1<<LED_BIT);
+
+    for (;;) {
+        LED_PORT |= (1<<LED_BIT);
+        _delay_ms(500);
+
+        LED_PORT &= ~(1<<LED_BIT);
+        _delay_ms(500);
+    }
+
+    return 0; // not reachable
+} // end of main
