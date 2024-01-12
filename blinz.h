5e83971d314f5f25fd11d4b310f768cad7902294
Jakub Šmejkal <smejkj20@fit.cvut.cz>
2024-01-12 18:48:13 +0100

    Blinz added libs a version

--- blinz.h
+++ blinz.h
@@ -1,29 +1,11 @@
-/*
-  grbl.h - main Grbl include file
-  Part of Grbl
+// Main blitz include file
 
-  Copyright (c) 2015 Sungeun K. Jeon
-
-  Grbl is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Grbl is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef grbl_h
-#define grbl_h
+#ifndef blinz
+#define blinz
 
 // Grbl versioning system
-#define GRBL_VERSION "0.9j"
-#define GRBL_VERSION_BUILD "20160726"
+#define BLINZ_VERSION "0.1"
+#define BLINZ_VERSION_BUILD "20240112"
 
 // Define standard libraries used by Blinz
 #include <avr/io.h>
